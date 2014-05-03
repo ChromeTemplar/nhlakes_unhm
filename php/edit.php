948dd8ec1b73e09d2a0c3824cd830d1e7ac13c84
dakotaheyman13 <dakotaheyman13@gmail.com>
2014-05-03 17:36:13 +0000

    Boater Survey Group - Verifies permissions for surveyEditingPage

--- php/edit.php
+++ php/edit.php
@@ -15,28 +15,28 @@ require("boaterSurvey.php");
   
   	<div id='cssmenu'>
 			<ul>
-			   <li><a href='index.php'><span>NHVBSR</span></a></li>
+			   <li><a href='survey.php'><span>NHVBSR</span></a></li>
 			   <li><a href='survey.php'><span>Survey</span></a></li>
-			   <li><a href='#'><span>View</span></a></li>
-			   <li><a href='edit.php'><span>Edit</span></a></li>
+			   <li><a href='view.php'><span>View</span></a></li>
+			   <li><a href='edit.php'><span>View/Edit</span></a></li>
 			   <li><a href='#'><span>Logout</span></a></li>
 			</ul>
 	</div>
 	<br><br><br>
     <div id="info">
-	<h2>Boater Survey</h2>
+	<h2>Boater Survey List</h2>
 	<hr>
       <?php
-		$user_id = 6;
+		$user_id = 3;
 		$model = new surveyModel();
 		$view = new surveyView();
-		$role = $model->getUserRole($user_id);
+		$role = $model->getUserRole($_SESSION['userid']);
 		
 		if($role == "StaffMember") {
 			echo "Your are a staff member";
 			header('Location: accessSitesPage.php');
 		} else {
-			$view->viewSurveysList($user_id);
+			$view->viewSurveysList($_SESSION['userid']);
 		}
 	  ?>
     </div>
