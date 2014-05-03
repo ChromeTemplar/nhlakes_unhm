163dc0c4cd7acef12daaf080fa061f098807ace9
dakotaheyman13 <dakotaheyman13@gmail.com>
2014-05-03 17:34:33 +0000

    Boater Survey Group - Page where survey can be edited

--- php/surveyEditingPage.php
+++ php/surveyEditingPage.php
@@ -14,24 +14,25 @@ require("boaterSurvey.php");
   <body>
 	<div id='cssmenu'>
 			<ul>
-			   <li><a href='index.php'><span>NHVBSR</span></a></li>
+			   <li><a href='survey.php'><span>NHVBSR</span></a></li>
 			   <li><a href='survey.php'><span>Survey</span></a></li>
-			   <li><a href='#'><span>View</span></a></li>
-			   <li><a href='edit.php'><span>Edit</span></a></li>
+			   <li><a href='view.php'><span>View</span></a></li>
+			   <li><a href='edit.php'><span>View/Edit</span></a></li>
 			   <li><a href='#'><span>Logout</span></a></li>
 			</ul>
 	</div>
   
 	<br><br><br>
     <div id="info">
-	<h2>Boater Survey</h2>
+	<h2>Edit/Flag Survey</h2>
 	<hr>
       <?php
-		$user_id = 6;
+	  
+		$user_id = 2;
 		if(isset($_POST['surveyID'])) {
 			$surveyID = $_POST['surveyID'];
 			$view = new surveyView();
-			$view->editSurvey($surveyID,$user_id);
+			$view->editSurvey($surveyID,$_SESSION['userid']);
 		} else {
 			echo "Please select a survey!";
 		}
