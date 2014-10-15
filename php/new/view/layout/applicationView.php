36706195321bdca87988f65c847ea6e1a4f62424
colby.johnson25 <colby.johnson25@gmail.com>
2014-10-15 20:53:58 +0000

    Colby implemented Jquery Theme

--- php/new/view/layout/applicationView.php
+++ php/new/view/layout/applicationView.php
@@ -2,8 +2,10 @@
     <head>
         <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/themes/smoothness/jquery-ui.css" />
         <link rel="stylesheet" href="assets/css/application.css" />
+        <link rel="stylesheet" href="assets/css/jquery-ui.theme.css" />
         <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
         <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min.js"></script>
+        <script src="assets/js/jquery-custom.js"></script>
     </head>
     
     <body>
