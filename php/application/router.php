db1b9ed443c7ee63f7dc97db27e20bab91caec54
sridraman <sridraman@gmail.com>
2014-12-12 00:45:00 +0000

    changes made for routing a new session to the logon screen, and validating the new session, routing a valid user id and password to the home screen.

--- php/application/router.php
+++ php/application/router.php
@@ -67,8 +67,8 @@ class router {
     public function loader()
     {
         /*** check the route ***/
-        $this->getController();
-
+        $this->getController();	
+        
         /*** if the file is not there die ***/
         if (is_readable($this->file) == false)
         {
@@ -113,34 +113,85 @@ class router {
 
         /*** get the route from the url ***/
         $route = (empty($_GET['rt'])) ? '' : $_GET['rt'];
-
-        if (empty($route))
-        {
-            $route = 'index';
-        }
-        else
-        {
-            /*** get the parts of the route ***/
-            $parts = explode('/', $route);
-            $this->controller = $parts[0];
-            if(isset( $parts[1]))
-            {
-                $this->action = $parts[1];
-            }
-        }
-
-        if (empty($this->controller))
-        {
-            $this->controller = 'home';
-        }
-
-        /*** Get action ***/
-        if (empty($this->action))
-        {
-            $this->action = 'index';
-        }
+		
+		/**
+		 * Before the userLogin the Login Screen is loaded 
+		 * 1. The index method in the LoginNHVBSRController class is executed
+		 * 2. After the userEnters the userId and password the validate 
+		 * method is executed 
+		 */
+        
+        $sessionVal = '';
+        
+		if (! (empty ( $route ))) {
+			
+			if (session_status() == PHP_SESSION_NONE) {
+				session_start ();
+			}
+			
+			$sessionVal = (empty ( $_SESSION ['IDKey'] )) ? '' : $_SESSION ['IDKey'];
+		} 
+        
+		if (empty ( $route ) && empty ( $sessionVal )) {
+			$this->action = 'index';
+			$this->controller = 'LoginNHVBSR';
+		} else if ((!empty ( $route )) && empty ( $sessionVal )) {
+			$this->action = 'validateUserId';
+			$this->controller = 'LoginNHVBSR';
+		} else if (! empty ( $sessionVal )) {
+			
+			//validates if the request to the webapp has a active sesion
+			//setup 
+			$this->validateActiveSession($sessionVal);
+			
+			if (empty ( $route )) {
+				$route = 'index';
+			} else {
+				/**
+				 * * get the parts of the route **
+				 */
+				$parts = explode ( '/', $route );
+				$this->controller = $parts [0];
+				if (isset ( $parts [1] )) {
+					$this->action = $parts [1];
+				}
+			}
+			
+			if (empty ( $this->controller )) {
+				$this->controller = 'home';
+			}
+			
+			/**
+			 * * Get action **
+			 */
+			if (empty ( $this->action )) {
+				$this->action = 'index';
+			}
+		}
 
         /*** set the file path ***/
         $this->file = $this->path .'/'. $this->controller . '.php';
+    }
+    
+    /***
+     * Below method will be called to check if the request to the
+     * webApp has a active session, if an active session is not 
+     * found error message is thrown 
+     * @param unknown $sessionVal
+     */
+    private function validateActiveSession($sessionVal) {
+    	
+    	//LoginNHVBSRController
+    	/*** include the controller ***/
+    	$klassLoc = $this->path . '/' . 'LoginNHVBSR.php';
+    	include $klassLoc;
+    	
+    	$klass = 'LoginNHVBSR' . 'Controller'; 
+    	$loginCntrller = new $klass($this->registry);
+    	if ($loginCntrller->checkActiveSession($sessionVal)) {
+    		return true;
+    	} else {
+    		return false;
+    	}
     }
 }
