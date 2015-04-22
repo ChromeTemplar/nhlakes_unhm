3b75ee4cf449941b5e7d3c77b2c894454781b690
kristacleary <kristacleary@gmail.com>
2015-04-22 23:14:24 +0000

    update report view

--- php/view/report/index.php
+++ php/view/report/index.php
@@ -1,4 +1,13 @@
 <h1><?php echo $welcome ?></h1>
 
-<br>
+<?php
+
+if ($report !="")
+	echo $this->buildTable($report);
+else
+	echo "<h2> There are no Survey Summaries in the system. </h2>";
+
+?><br>
 Return <?php echo $this->linkTo("home","index","Home"); ?>
+
+
