a4f8c600fda72f00f20d1270d8ad0e60ae9d474a
kristacleary <kristacleary@gmail.com>
2015-04-22 23:13:59 +0000

    update report model

--- php/model/report.php
+++ php/model/report.php
@@ -2,6 +2,24 @@
 
 class report extends Model
 {
+    /*** Set Class Attribute Variables ***/
+    var $host = "localhost";
+    var $user = "root";
+    var $pass = '';
+    var $db = "NHVBSR";
+
+    /**
+    * Constructor
+    **/
+    function __construct($id ="")
+    {   
+        /*** Set table name ***/
+        if (empty($this->table)) {  
+            $this->table = "summary"; 
+        }         
+
+        /*** Create Connection to DB ***/
+       $this->conn = mysqli_connect($this->host, $this->user, $this->pass, $this->db) or $this->error('Could not connect to database. Make sure settings are correct.'); 
+    } 
     
 }
-
