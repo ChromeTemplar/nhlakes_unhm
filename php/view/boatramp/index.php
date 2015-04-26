e65097d66e30c3331141deb93a199758f9da42ef
consolijulian <consolijulian@gmail.com>
2015-04-26 04:48:14 +0000

    Modified buildTable function and added controller parameter to it. That's it. Your modified code is safe. :)

---
+++ php/view/boatramp/index.php
@@ -0,0 +1,38 @@
+<h1><?php echo $welcome; ?></h1>
+
+
+<?php if ($ramps != ""){ ?>
+
+    <!-- demo -->
+    <div id="data">
+
+        <!-- panel -->
+        <?php 
+        
+        require_once "view/partials/_controlsTop.php";				 
+        echo "<div id='boatramp-table' >".$this->buildRampTable($ramps, "ramps"). "</div>";
+        require_once "view/partials/_controlsBottom.php";
+        
+        ?>
+        <!-- no results found -->
+        <div class="jplist-no-results">
+          <p>No results found</p>
+        </div>
+
+    </div>
+    <?php
+    
+}else
+    echo "<h2>No Boat Ramps Found</h2>";
+
+?>
+<div id="content-bottom">
+	<?php if (isset($_SESSION['roleID']) && ($_SESSION['roleID'] < 3)) { ?>
+	    <?php echo $this->buttonTo("boatramp","newBoatRamp", "New"); ?>
+	<?php  } ?>
+    <br/>
+    Return <?php echo $this->linkTo("home","index","Home"); ?>
+</div>
+
+
+
