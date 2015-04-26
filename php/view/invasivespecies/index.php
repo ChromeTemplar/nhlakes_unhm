9a17ae67fd0f5910e0590b59f7c477c2ab662e5b
consolijulian <consolijulian@gmail.com>
2015-04-26 04:51:27 +0000

    Modified buildTable function and added controller parameter to it. That's it. Your modified code is still in tact. Thanks for helping out Invasive Species team. :)

---
+++ php/view/invasivespecies/index.php
@@ -0,0 +1,16 @@
+<h1><?php echo $welcome ?></h1>
+
+<?php
+include 'view/lakegroupstats/surveytotalview.php';
+if ($invasivespecies !="")        
+    echo $this->buildTable($invasivespecies, "invasivespecies");
+else
+    echo "<h2>No Invasive Species Found </h2>";
+    
+echo $this->buttonTo("invasivespecies","newInvasiveSurvey", "New");
+
+
+
+
+?><br>
+Return <?php echo $this->linkTo("home","index","Home"); ?>
