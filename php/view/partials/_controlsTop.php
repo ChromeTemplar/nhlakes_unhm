a2a290338c91f3256bda32387f8bed8573ddc109
consolijulian <consolijulian@gmail.com>
2015-04-26 04:45:54 +0000

    Fixed a CSS issue that affected the orientation of the tables.

---
+++ php/view/partials/_controlsTop.php
@@ -0,0 +1,45 @@
+<div class="jplist-panel">						
+    <!-- items per page dropdown -->
+    <div 
+       class="jplist-drop-down" 
+       data-control-type="items-per-page-drop-down" 
+       data-control-name="paging" 
+       data-control-action="paging">
+
+       <ul>
+         <li><span data-number="3"> 3 per page </span></li>
+         <li><span data-number="5"> 5 per page </span></li>
+         <li><span data-number="10" data-default="true"> 10 per page </span></li>
+         <li><span data-number="all"> view all </span></li>
+       </ul>
+    </div>
+
+    <!-- ios button: show/hide panel -->
+    <div class="jplist-ios-button">
+       <i class="fa fa-sort"></i>
+       jPList Actions
+    </div>
+
+    <!-- filter by title -->
+    <div class="text-filter-box">
+
+      <i class="fa fa-search jplist-icon"></i>
+
+      <!--[if lt IE 10]>
+      <div class="jplist-label">Filter by Title:</div>
+      <![endif]-->
+
+      <input 
+        data-path=".title" 
+        type="text" 
+        value="" 
+        placeholder="Filter by Name" 
+        data-control-type="textbox" 
+        data-control-name="title-filter" 
+        data-control-action="filter"
+      />
+    </div>	
+      
+</div>
+<br/><br/>
+
