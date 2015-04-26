10b82f9887ed84f703ac92da9a51a590baa106b3
consolijulian <consolijulian@gmail.com>
2015-04-26 04:36:44 +0000

    Modified buildTable function and added controller parameter to it. That's it.

---
+++ php/view/user/index.php
@@ -0,0 +1,15 @@
+<h1><?php echo $welcome ?></h1>
+
+<?php
+
+if ($user !="")
+	echo $this->buildTable($user, "user");
+else
+	echo "<h2> There are no Lake Hosts in the system. </h2>";
+
+echo $this->buttonTo("user","newuser", "New");
+
+?><br>
+Return <?php echo $this->linkTo("home","index","Home"); ?>
+
+
