7e4293a06a27216b683c021470daf831138e91c2
consolijulian <consolijulian@gmail.com>
2015-04-26 04:39:20 +0000

    Fixed the add and edit so it doesn't return sql errors.

---
+++ php/model/user.php
@@ -0,0 +1,201 @@
+<?php
+
+class user extends Model
+{
+    /*** Set Class Attribute Variables ***/
+    var $host = "localhost";
+    var $user = "root";
+    var $pass = '';
+    var $db = "NHVBSR";
+
+    /**
+    * Constructor
+    **/
+    function __construct($id ="")
+    {   
+        /*** Set table name ***/
+        if (empty($this->table)) {  
+            $this->table = get_class($this); 
+        }         
+         
+         /*** Set ID ***/
+        if (!empty($id)) { 
+            $this->id = $id; 
+        } 
+
+        /*** Create Connection to DB ***/
+       $this->conn = mysqli_connect($this->host, $this->user, $this->pass, $this->db) or $this->error('Could not connect to database. Make sure settings are correct.'); 
+    } 
+
+    /**
+    * Adds a new Person to the DB with the given $data
+    *
+    * @param Array $data : Array containing all of the $_POST data passed in by form
+    *
+    **/
+    function adduser($data)
+    {
+        $mysqli = $this->conn;
+        
+        if (empty($table)) 
+            $table = $this->table;
+
+        /* Prepared statement, stage 1: prepare */
+        if (!($stmt = $mysqli->prepare("INSERT INTO user (
+		
+			roleID,
+        	coordinatorID,	 
+			firstName, 
+			lastName, 
+			phoneNumber, 
+			userName, 
+			email, 
+			password, 
+			over18,
+			verified) 
+			
+			VALUES (?,?,?,?,?,?,?,SHA1(?),?,?)"))) {
+			
+            echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+        }
+		// Creates array of firstName & lastName
+		$userNameArray = array($data['firstName'], $data['lastName']);
+		// Combines firstName & lastName and adds a . in the middle
+		$userName = implode(".",$userNameArray);
+		//$userName is created as firstName.lastName
+		
+		// Creates array for phone number using areaCode, phoneBegin, phoneEnd
+		$phoneNumberArray = array($data['areaCode'], $data['phoneBegin'], $data['phoneEnd']); 
+		// Combines areaCode, phoneBegin, and phoneEnd and adds hyphens inbetween to produce a phone number
+		$phoneNumber = implode("-",$phoneNumberArray);
+		
+        /* Prepared statement, stage 2: bind and execute */
+        if (!($stmt->bind_param("iissssssii",
+		
+			$data['roleID'],
+        	$data['coordinatorID'],	
+			$data['firstName'],
+			$data['lastName'],
+			$phoneNumber,
+			$userName,
+			$data['email'],
+			$data['password'],
+			$data['over18'],
+			$data['verified']
+									))) {
+            echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+        }
+        if (!$stmt->execute()) {
+            echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+        }
+    }
+
+
+    /**
+    * Selects a single item if ID is set 
+    **/
+    function at_id() 
+    {
+        $mysqli = $this->conn;
+
+        /* Prepared statement, stage 1: prepare */
+        if (!($stmt = $mysqli->prepare("SELECT * FROM user WHERE ID = ?"))) {
+            echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+        }
+
+        /* Prepared statement, stage 2: bind and execute */
+        if (!($stmt->bind_param("i", $this->id))) {
+            echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+        }
+
+        return $this->process($stmt);
+    }
+
+
+    /**
+    * Updates a Person row in the DB
+    *
+    * @param Array $data : Array containing $_POST data passed in by the form
+    **/
+    function updateuser($data) 
+    {
+        $mysqli = $this->conn;
+        
+        /* Prepared statement, stage 1: prepare */
+        if (!($stmt = $mysqli->prepare("UPDATE user SET 
+				
+				roleID = ?,
+        		coordinatorID = ?, 
+				firstName = ?,
+				lastName = ?, 
+				phoneNumber = ?, 
+				userName = ?, 
+				email = ?, 
+				password = SHA1(?), 
+				over18 = ?, 
+				verified = ? 
+										WHERE ID = ?"))) {
+										
+            echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+        }
+        
+        // Creates array of firstName & lastName
+        $userNameArray = array($data['firstName'], $data['lastName']);
+        // Combines firstName & lastName and adds a . in the middle
+        $userName = implode(".",$userNameArray);
+        //$userName is created as firstName.lastName
+        
+        // Creates array for phone number using areaCode, phoneBegin, phoneEnd
+        $phoneNumberArray = array($data['areaCode'], $data['phoneBegin'], $data['phoneEnd']);
+        // Combines areaCode, phoneBegin, and phoneEnd and adds hyphens inbetween to produce a phone number
+        $phoneNumber = implode("-",$phoneNumberArray);        
+        
+        /* Prepared statement, stage 2: bind and execute */
+        if (!($stmt->bind_param("iissssssiii",
+		
+				$data['roleID'],
+        		$data['coordinatorID'],
+				$data['firstName'],
+				$data['lastName'],
+				$phoneNumber,
+				$userName,
+				$data['email'],
+				$data['password'],
+				$data['over18'],
+				$data['verified'],
+				
+								$this->id))) {
+								
+            echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+        }
+
+        if (!$stmt->execute()) {
+            echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+        }
+    }
+
+    /**
+    * Deletes a User from the DB
+    **/
+    function deleteuser() 
+    {
+        $mysqli = $this->conn;
+        
+        /* Prepared statement, stage 1: prepare */
+        if (!($stmt = $mysqli->prepare("DELETE FROM user WHERE ID = ?"))) {
+            echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+        }
+
+        /* Prepared statement, stage 2: bind and execute */
+        if (!($stmt->bind_param("i", $this->id))) {
+            echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+        }
+
+        if (!$stmt->execute()) {
+            echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+        }  
+    }
+
+    
+    
+}
