7fa869817928518300d2b2f96ca031a4cc927257
kristacleary <kristacleary@gmail.com>
2015-05-01 00:27:41 +0000

    updated report model

--- php/model/report.php
+++ php/model/report.php
@@ -591,5 +591,180 @@ class report extends Model
         
         return $this->process($stmt);
     }
+	
+	///////////////////////////////////////////
+	///////////////////////////////////////////
+	// Section 4 - All Summaries By Date///////
+	///////////////////////////////////////////
+	///////////////////////////////////////////
+	
+	
+	// Displays the totals for all States. Sums the summary entries to create totals by state.
+    function allDateStates($table = '', $cols= '	DATE_FORMAT(summaryDate,"%m/%d/%Y") as "Date", 
+												SUM(NH) as NH, 
+												SUM(ME) as ME,
+												SUM(MA) as MA, 
+												SUM(VT) as VT, 
+												SUM(CT) as CT, 
+												SUM(RI) as RI,
+												SUM(NY) as NY,
+												SUM(other) as Other
+												') {
+        $mysqli = $this->conn;
+		
+		
+		
+        if (empty($table)) 
+            $table = $this->table;
+
+        /* Prepared statement, stage 1: prepare. Selects data from the summary table to be displayed on the report page. */ 
+        if (!($stmt = $mysqli->prepare("Select $cols FROM $table WHERE YEAR(summaryDate) = YEAR(Now()) GROUP BY summaryDate"))) {
+            echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+        }
+        
+        return $this->process($stmt);
+    }
+	
+	// Shows totals for all Boat Types
+    function allDateBoatTypes($table = '', $cols= '	DATE_FORMAT(summaryDate,"%m/%d/%Y") as "Date", SUM(inboardOutboard) as "I/O", 
+												SUM(pwc) as "PWC Jet",
+												SUM(canoeKayak) as "Canoe/Kayak",
+												SUM(sail) as "Sail Boat",
+												SUM(otherBoatType) as "Other"
+												') {
+        $mysqli = $this->conn;
+
+        if (empty($table)) 
+            $table = $this->table;
+
+        /* Prepared statement, stage 1: prepare. Selects data from the summary table to be displayed on the report page. */ 
+        if (!($stmt = $mysqli->prepare("Select $cols FROM $table WHERE YEAR(summaryDate) = YEAR(Now()) GROUP BY summaryDate"))) {
+            echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+        }
+        
+        return $this->process($stmt);
+    }
+
+	// Show totals for Previously Interacted and Never Interacted.
+	function allDatePreviousInteractions($table = '', $cols= '	DATE_FORMAT(summaryDate,"%m/%d/%Y") as "Date", SUM(previous) as "Previously Interacted", 
+												SUM(notPrevious) as "Never Interacted"
+												') {
+        $mysqli = $this->conn;
+
+        if (empty($table)) 
+            $table = $this->table;
+
+        /* Prepared statement, stage 1: prepare. Selects data from the summary table to be displayed on the report page. */ 
+        if (!($stmt = $mysqli->prepare("Select $cols FROM $table WHERE YEAR(summaryDate) = YEAR(Now()) GROUP BY summaryDate"))) {
+            echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+        }
+        
+        return $this->process($stmt);
+    }
+	
+	// Displays totals for Drained and Not Drained.
+	function allDateDrained($table = '', $cols= '	DATE_FORMAT(summaryDate,"%m/%d/%Y") as "Date", SUM(drained) as "Drained", 
+												SUM(notDrained) as "Not Drained"
+												') {
+        $mysqli = $this->conn;
+
+        if (empty($table)) 
+            $table = $this->table;
+
+        /* Prepared statement, stage 1: prepare. Selects data from the summary table to be displayed on the report page. */ 
+        if (!($stmt = $mysqli->prepare("Select $cols FROM $table WHERE YEAR(summaryDate) = YEAR(Now()) GROUP BY summaryDate"))) {
+            echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+        }
+        
+        return $this->process($stmt);
+    }
+	
+	// Displays totals for Rinsed and Not rinsed.
+	function allDateRinsed($table = '', $cols= '	DATE_FORMAT(summaryDate,"%m/%d/%Y") as "Date", SUM(rinsed) as "Rinsed", 
+												SUM(notRinsed) as "Not Rinsed"
+												
+												') {
+        $mysqli = $this->conn;
+
+        if (empty($table)) 
+            $table = $this->table;
+
+        /* Prepared statement, stage 1: prepare. Selects data from the summary table to be displayed on the report page. */ 
+        if (!($stmt = $mysqli->prepare("Select $cols FROM $table WHERE YEAR(summaryDate) = YEAR(Now()) GROUP BY summaryDate"))) {
+            echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+        }
+        
+        return $this->process($stmt);
+    }
+	
+	// Displays totals for Dried and Not dried.
+	function allDateDried($table = '', $cols= '	DATE_FORMAT(summaryDate,"%m/%d/%Y") as "Date", SUM(dry5) as "Dried", 
+												SUM(notDry5) as "Not Dried"
+												') {
+        $mysqli = $this->conn;
+
+        if (empty($table)) 
+            $table = $this->table;
+
+        /* Prepared statement, stage 1: prepare. Selects data from the summary table to be displayed on the report page. */ 
+        if (!($stmt = $mysqli->prepare("Select $cols FROM $table WHERE YEAR(summaryDate) = YEAR(Now()) GROUP BY summaryDate"))) {
+            echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+        }
+        
+        return $this->process($stmt);
+    }
+	
+	// Displays totals for Low Awareness, Medium Awareness, High Awareness.
+	function allDateAwareness($table = '', $cols= '	DATE_FORMAT(summaryDate,"%m/%d/%Y") as "Date", SUM(awarenessLow) as "Low Awareness", 
+												SUM(awarenessMedium) as "Medium Awareness", 
+												SUM(awarenessHigh) as "High Awareness"
+												') {
+        $mysqli = $this->conn;
+
+        if (empty($table)) 
+            $table = $this->table;
+
+        /* Prepared statement, stage 1: prepare. Selects data from the summary table to be displayed on the report page. */ 
+        if (!($stmt = $mysqli->prepare("Select $cols FROM $table WHERE YEAR(summaryDate) = YEAR(Now()) GROUP BY summaryDate"))) {
+            echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+        }
+        
+        return $this->process($stmt);
+    }
+	
+	// Displays totals for Species Found and Species Not Found.
+	function allDateSpeciesFound($table = '', $cols= '	DATE_FORMAT(summaryDate,"%m/%d/%Y") as "Date", SUM(speciesFoundYes) as "Species Found", 
+												SUM(speciesFoundNo) as "Species Not Found"
+												') {
+        $mysqli = $this->conn;
+
+        if (empty($table)) 
+            $table = $this->table;
+
+        /* Prepared statement, stage 1: prepare. Selects data from the summary table to be displayed on the report page. */ 
+        if (!($stmt = $mysqli->prepare("Select $cols FROM $table WHERE YEAR(summaryDate) = YEAR(Now()) GROUP BY summaryDate"))) {
+            echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+        }
+        
+        return $this->process($stmt);
+    }
+	
+	// Displays totals for Sent to DES and Not Sent to DES.
+	function allDateSpecimenSent($table = '', $cols= 'DATE_FORMAT(summaryDate,"%m/%d/%Y") as "Date", SUM(sentDesYes) as "Sent to DES", 
+												SUM(sentDesNo) as "Not Sent to DES"
+												') {
+        $mysqli = $this->conn;
+
+        if (empty($table)) 
+            $table = $this->table;
+
+        /* Prepared statement, stage 1: prepare. Selects data from the summary table to be displayed on the report page. */ 
+        if (!($stmt = $mysqli->prepare("Select $cols FROM $table WHERE YEAR(summaryDate) = YEAR(Now()) GROUP BY summaryDate"))) {
+            echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+        }
+        
+        return $this->process($stmt);
+    }
+
     
 }
