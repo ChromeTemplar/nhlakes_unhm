8fc5354b1115f2e01740bcef117e7c31ffb656d3
tyler-lanning <tsldew@aol.com>
2015-12-07 21:12:55 -0500

    All of our previous changes got reverted by Camden's view update. Also adding an eight character minimum to the password field.

--- php/view/user/_form.php
+++ php/view/user/_form.php
@@ -39,18 +39,23 @@ else
     <select name="user[roleID]" required>
         <option selected disabled>-- Select Role --</option>
         <?php if ($_SESSION['roleID'] == 1) : ?>
-            <option value="1" <?php if (isset($user)) echo "value='$roleID'"; ?>> Administrator</option>
-            <option value="2" <?php if (isset($user)) echo "value='$roleID'"; ?>> Group Coordinator</option>
-            <option value="3" <?php if (isset($user)) echo "value='$roleID'"; ?>> Lake Host</option>
+            <option value="1" <?php if ($roleID == "1") echo "selected"; ?>> Administrator</option>
+            <option value="2" <?php if ($roleID == "2") echo "selected"; ?>> Group Coordinator</option>
+            <option value="3" <?php if ($roleID == "3") echo "selected"; ?>> Lake Host</option>
         <?php else : ?>
-            <option value="2" <?php if (isset($user)) echo "value='$roleID'"; ?>> Group Coordinator</option>
-            <option value="3" <?php if (isset($user)) echo "value='$roleID'"; ?>> Lake Host</option>
+            <option value="2" <?php if ($roleID == "2") echo "selected"; ?>> Group Coordinator</option>
+            <option value="3" <?php if ($roleID == "3") echo "selected"; ?>> Lake Host</option>
         <?php endif; ?>
     </select><br/><br/>
 
-    <!-- Coordinator ID -->
-    <label for="coordinatorID">Coordinator ID</label><br/>
-    <input type="text" name="user[coordinatorID]" class="medium" maxlength='1'><br/><br/>
+    <!-- Groups -->
+    <label for="coordinatorID">Group(s)</label><br/>
+    <select name="user[coordinatorID][]" multiple width="50" required>
+        <?php
+        $model = new group();
+        echo $model->fillSelectMultiple($user);
+        ?>
+    </select><br/><br/>
 
     <!-- Fist Name -->
     <label for="firstName">First Name</label><br/>
@@ -79,7 +84,7 @@ else
 
     <!--  Password -->
     <label for="password">Password</label><br/>
-    <input type="text" name="user[password]" class="medium" required><br/><br/>
+    <input type="password" name="user[password]" class="medium" pattern=".{8,}" required title="Password must be at least 8 characters"><br/><br/>
 
     <?php /*######### ######## DEPRICATED ######## ########
 	<!-- Over 18 -->
