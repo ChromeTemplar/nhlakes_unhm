ed76163621aa1317ce0f5efde022a9d5c45ce1f6
kitala1 <severanceday@hotmail.com>
2015-12-09 18:09:21 -0500

    Survey Summary now checks user permissions and fills in the "Lake Host Name" field automatically with the current username if that user is not a group coordinator or administrator.

--- php/view/surveysummary/_form.php
+++ php/view/surveysummary/_form.php
@@ -1,4 +1,3 @@
-
 <?php
 if (isset($summary)) 
 {
@@ -31,32 +30,52 @@ else
     ?> method="post">
     
 	<!-- THIS IS THE START OF THE HEADER FOR THE SURVEY SUMMARY -->
-	<div style="white-space: nowrap;"><h3>
-	<?php echo "NH LAKES 2014 Lake Host Program Ramp Daily Summary Sheet"; ?>
-	</h3></div>
+	<div style="white-space: nowrap;"><h2><b>
+	<?php echo "NH LAKES 2015 Lake Host Daily Summary Sheet"; ?>
+	</h2></b></div>
 		
-	<div style="white-space: nowrap;"><h5>
+	<div style="white-space: nowrap;"><hr>
+	<h5>
 	Date (YYYY-M-D): 		
 	<input type='date' name='summary[summaryDate]' 
-		<?php
-$date = new DateTime();
-
-echo "value='".$date->format('Y-m-d')."'";
-?>>
+	<?php
+	$date = new DateTime();
 	
-
+	echo "value='".$date->format('Y-m-d')."'";
+	?>>
 	
 	
-		
 	<!-- FIXME the lake host should be a fixed read only field if a lake host is logged in, if a group leader is logged in, then
 	they can select the users in their group, if admin is logged in they can select any lake host. 
 	so need to implement the fixed if(permission == lakehost) and if (permission == admin) etc... -->
-	Lake Host Name:<?php echo $this->selectList($lakeHostNames, 
-			array("name" => "summary[lakeHostName]", "id" => "lakeHostName", "class" => "medium selectmenu"),$lakeHostName); ?>
+	
+	<!-- Update 8 DEC 2015, the above FIXME should now be functioning properly. Leaving these comments in the code to 
+	help future developers -->
+	
+	Lake Host Name:
+	
+	<?php 
+	
+	if ($_SESSION['roleID'] == 1 || $_SESSION['roleID'] == 2)
+	{
+		echo $this->selectList($lakeHostNames, 
+				array("name" => "summary[lakeHostName]", "id" => "lakeHostName", "class" => "medium selectmenu"),
+				$lakeHostName); 
+	}
+	else 
+	{
+		echo ($_SESSION['userName']);
+	}?>
 	</h5></div>
 	
 	<div style="white-space: nowrap;"><h5>
-	1<sup>st</sup>Shift Start Time (H:MM):<input type='datetime' name='summary[startShiftTime]'
+		Is this a summary for an entire work day or a single shift?
+		<input onclick="document.getElementById('startTime').disabled = true; document.getElementById('endTime').disabled = true;" type='radio' name='fullDay' id='fullDayYes' value='yes'>Full day
+		<input onclick="document.getElementById('startTime').disabled = false; document.getElementById('endTime').disabled = false;" type='radio' name='fullDay' id='fullDayNo' value='no'>Single shift
+	</h5></div>
+	
+	<div style="white-space: nowrap;"><h5>
+	1<sup>st</sup>Shift Start Time (H:MM):<input type='time' name='summary[startShiftTime]' id='startTime'
 		<?php
 		if (isset($summary))
 		{
@@ -65,7 +84,7 @@ echo "value='".$date->format('Y-m-d')."'";
 			$time = substr($summary['startShiftTime'], $pos + 1);
 			echo "value='".$time."'";
 		}?>>
-	Last Shift End Time (H:MM):<input type='datetime' name='summary[endShiftTime]'
+	Last Shift End Time (H:MM):<input type='time' name='summary[endShiftTime]' id='endTime'
 		<?php 
 		if (isset($summary)) 
 		{
@@ -83,10 +102,9 @@ echo "value='".$date->format('Y-m-d')."'";
 	<!-- waterbody is not stored in the summary database table, it is intended to help filter the boat ramp selection -->
 	Waterbody: <?php echo $this->selectList($waterbodies,
 			array("name" => "summary[waterbody]", "id" => "waterbody", "class" => "medium selectmenu"),$waterbody); ?>
-	</h5></div>
-	
-	<div style="white-space: nowrap;"><h5>
 	<!-- town is not stored in the database summary table, it is intended to help filter the boat ramp selection -->
+	</div></h5>
+	<div style="white-space: nowrap;"><h5>
 	Town: <?php echo $this->selectList($towns, 
 			array("name" => "summary[town]", "id" => "town", "class" => "medium selectmenu"),$town); ?>
 	Ramp Name: <?php echo $this->selectList($rampNames, array("name" => "summary[boatRampName]",
@@ -98,16 +116,36 @@ echo "value='".$date->format('Y-m-d')."'";
 Number Inspected
 <input type='number' name='summary[totalInspections]'
 <?php if (isset($summary)) echo "value='".$summary['totalInspections']."'"; ?> />
-<br><br>
-<u>Totals</u>
-<br><br>
+<br></br><hr>
+<u><h2><b>Totals</b></h2></h2></u>
+<br></br>
 <strong>State Boat Registration</strong>
 <table  border="0" cellpadding="1" cellspacing="1" >
-	<tr >
+	<tr>
 		<td>NH </td>
 		<td>MA<br>(MS)</td>
 		<td>ME </td>
 		<td>VT </td>
+	</tr>
+	<tr>
+		<td>
+			<input type='number' name='summary[NH]' min='0'
+			<?php if (isset($summary)) echo "value='".$summary['NH']."'"; ?> />
+		</td>
+		<td>
+			<input type='number' name='summary[MA]' min='0'
+			<?php if (isset($summary)) echo "value='".$summary['MA']."'"; ?> />
+		</td>
+		<td>
+			<input type='number' name='summary[ME]' min='0'
+			<?php if (isset($summary)) echo "value='".$summary['ME']."'"; ?> />
+		</td>
+		<td>
+			<input type='number' name='summary[VT]' min='0'
+			<?php if (isset($summary)) echo "value='".$summary['VT']."'"; ?> />
+		</td>
+	</tr>
+	<tr>
 		<td>CT </td>
 		<td>RI </td>
 		<td>NY </td>
@@ -115,35 +153,19 @@ Number Inspected
 	</tr>
 	<tr>
 		<td>
-			<input type='number' name='summary[NH]'  
-			<?php if (isset($summary)) echo "value='".$summary['NH']."'"; ?> />
-		</td>
-		<td>
-			<input type='number' name='summary[MA]'  
-			<?php if (isset($summary)) echo "value='".$summary['MA']."'"; ?> />
-		</td>
-		<td>
-			<input type='number' name='summary[ME]'  
-			<?php if (isset($summary)) echo "value='".$summary['ME']."'"; ?> />
-		</td>
-		<td>
-			<input type='number' name='summary[VT]'  
-			<?php if (isset($summary)) echo "value='".$summary['VT']."'"; ?> />
-		</td>
-		<td>
-			<input type='number' name='summary[CT]'  
+		<input type='number' name='summary[CT]' min='0'
 			<?php if (isset($summary)) echo "value='".$summary['CT']."'"; ?> />
 		</td>
 		<td>
-			<input type='number' name='summary[RI]'  
+			<input type='number' name='summary[RI]' min='0'
 			<?php if (isset($summary)) echo "value='".$summary['RI']."'"; ?> />
 		</td>
 		<td>
-			<input type='number' name='summary[NY]'
+			<input type='number' name='summary[NY]' min='0'
 			<?php if (isset($summary)) echo "value='".$summary['NY']."'"; ?> />
 		</td>
 		<td>
-			<input type='number' name='summary[other]'  
+			<input type='number' name='summary[other]' min='0'
 			<?php if (isset($summary)) echo "value='".$summary['other']."'"; ?> />
 		</td>
 	</tr>
@@ -160,23 +182,23 @@ Number Inspected
 	</tr>
 	<tr>
 	    <td>
-			<input type='number' name='summary[inboardOutboard]'  
+			<input type='number' name='summary[inboardOutboard]' min='0'
 			<?php if (isset($summary)) echo "value='".$summary['inboardOutboard']."'"; ?> />
 		</td>
 		<td>
-			<input type='number' name='summary[pwc]'  
+			<input type='number' name='summary[pwc]' min='0'
 			<?php if (isset($summary)) echo "value='".$summary['pwc']."'"; ?> />
 		</td>
 		<td>
-			<input type='number' name='summary[canoeKayak]'  
+			<input type='number' name='summary[canoeKayak]' min='0' 
 			<?php if (isset($summary)) echo "value='".$summary['canoeKayak']."'"; ?> />
 		</td>
 		<td>
-			<input type='number' name='summary[sail]'  
+			<input type='number' name='summary[sail]' min='0'
 			<?php if (isset($summary)) echo "value='".$summary['sail']."'"; ?> />
 		</td>
 		<td>
-			<input type='number' name='summary[otherBoatType]'  
+			<input type='number' name='summary[otherBoatType]' min='0'  
 			<?php if (isset($summary)) echo "value='".$summary['otherBoatType']."'"; ?> />
 		</td>
 	</tr>
@@ -190,11 +212,11 @@ Number Inspected
 	</tr>
 	<tr>
 		<td>
-			<input type='number' name='summary[previous]'  
+			<input type='number' name='summary[previous]' min='0'
 			<?php if (isset($summary)) echo "value='".$summary['previous']."'"; ?> />
 		</td>
 		<td>
-			<input type='number' name= 'summary[notPrevious]'    
+			<input type='number' name= 'summary[notPrevious]' min='0'    
 			<?php if (isset($summary)) echo "value='".$summary['notPrevious']."'"; ?> />
 		</td>
 	</tr>
@@ -208,11 +230,11 @@ Number Inspected
 	</tr>
 	<tr>
 		<td>
-			<input type='number' name='summary[drained]'  
+			<input type='number' name='summary[drained]' min='0'
 			<?php if (isset($summary)) echo "value='".$summary['drained']."'"; ?> />
 		</td>
 		<td>
-			<input type='number' name='summary[notDrained]'  
+			<input type='number' name='summary[notDrained]' min='0'
 			<?php if (isset($summary)) echo "value='".$summary['notDrained']."'"; ?> />
 		</td>
 	</tr>
@@ -226,11 +248,11 @@ Number Inspected
 	</tr>
 	<tr>
 		<td>
-			<input type='number' name='summary[rinsed]'  
+			<input type='number' name='summary[rinsed]' min='0'
 			<?php if (isset($summary)) echo "value='".$summary['rinsed']."'"; ?> />
 		</td>
 		<td>
-			<input type="number" name='summary[notRinsed]'  
+			<input type="number" name='summary[notRinsed]' min='0'
 			<?php if (isset($summary)) echo "value='".$summary['notRinsed']."'"; ?> />
 		</td>
 	</tr>
@@ -244,11 +266,11 @@ Number Inspected
 	</tr>
 	<tr>
 		<td>
-			<input type='number' name='summary[dry5]'  
+			<input type='number' name='summary[dry5]' min='0'
 			<?php if (isset($summary)) echo "value='".$summary['dry5']."'"; ?> />
 		</td>
 		<td>
-			<input type='number' name='summary[notDry5]'  
+			<input type='number' name='summary[notDry5]' min='0'
 			<?php if (isset($summary)) echo "value='".$summary['notDry5']."'"; ?> />
 		</td>
 	</tr>
@@ -263,15 +285,15 @@ Number Inspected
 	</tr>
 	<tr>
 		<td>
-			<input type='number' name='summary[awarenessHigh]'  
+			<input type='number' name='summary[awarenessHigh]' min='0'
 			<?php if (isset($summary)) echo "value='".$summary['awarenessHigh']."'"; ?> />
 		</td>
 		<td>
-			<input type='number' name='summary[awarenessMedium]'  
+			<input type='number' name='summary[awarenessMedium]' min='0'
 			<?php if (isset($summary)) echo "value='".$summary['awarenessMedium']."'"; ?> />
 		</td>
 		<td>
-			<input type='number' name='summary[awarenessLow]'  
+			<input type='number' name='summary[awarenessLow]' min='0'
 			<?php if (isset($summary)) echo "value='".$summary['awarenessLow']."'"; ?> />
 		</td>
 	</tr>
@@ -285,35 +307,38 @@ Number Inspected
 	</tr>
 	<tr>
 		<td>
-			<input type='number' name='summary[speciesFoundYes]'  
+			<input type='number' name='summary[speciesFoundYes]' min='0'
 			<?php if (isset($summary)) echo "value='".$summary['speciesFoundYes']."'"; ?> />
 		</td>
 		<td>
-			<input type='number' name='summary[speciesFoundNo]'  
+			<input type='number' name='summary[speciesFoundNo]' min='0'
 			<?php if (isset($summary)) echo "value='".$summary['speciesFoundNo']."'"; ?> />
 		</td>
 	</tr>
 </table>
 
-<strong>Sent to DES?</strong>
-<table  border="0" cellpadding="1" cellspacing="1">
-	<tr>
-		<td>Yes</td>
-		<td>No</td>
-	</tr>
-	<tr>
-		<td>
-			<input type='number' name='summary[sentDesYes]'  
-			<?php if (isset($summary)) echo "value='".$summary['sentDesYes']."'"; ?> />
-		</td>
-		<td>
-			<input type='number' name='summary[sentDesNo]'  
-			<?php if (isset($summary)) echo "value='".$summary['sentDesNo']."'"; ?> />
-		</td>
-	</tr>
-</table>
+<!-- <strong>Sent to DES?</strong> -->
+<!-- <table  border="0" cellpadding="1" cellspacing="1"> -->
+<!-- 	<tr> -->
+<!-- 		<td>Yes</td> -->
+<!-- 		<td>No</td> -->
+<!-- 	</tr> -->
+<!-- 	<tr> -->
+<!-- 		<td> -->
+<!-- 			<input type='number' name='summary[sentDesYes]' min='0' -->
+			<?php // if (isset($summary)) echo "value='".$summary['sentDesYes']."'"; ?> 
+			<!-- /> -->
+<!-- 		</td> -->
+<!-- 		<td> -->
+<!-- 			<input type='number' name='summary[sentDesNo]' min='0' -->
+			<?php // if (isset($summary)) echo "value='".$summary['sentDesNo']."'"; ?> 
+			<!-- /> -->
+<!-- 		</td> -->
+<!-- 	</tr> -->
+<!-- </table> -->
 	
 
 </br>
 <input type='submit' value='Submit' />
-</form>    
+</form>
+  
