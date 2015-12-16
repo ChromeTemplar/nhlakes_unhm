f59d29ca05c95bc0ebad73a2fa53b4ed95557cea
AvenZrk <Aven.Zrk@gmail.com>
2015-12-15 20:49:48 -0500

    Corrected Nav buttons

--- php/view/partials/_navigation.php
+++ php/view/partials/_navigation.php
@@ -1,38 +1,46 @@
-<!--  Remove CSS Segregation -->
-<!-- <div id="navigation-title">Menu1</div>  -->
 
-<!--<h1>Menu</h1>-->
-
+<html>
+<body>
 <div id="nav">
 
-    <?php echo "EOA ON" ?> <!-- SHOULD POINT HERE --> <!--a href="view/layout/applicationViewEOA.php"-->
+			<table align="center">
+				  <!--<td class="button"><?php /*echo "EOA ON" */?></td> <!-- SHOULD POINT HERE --> <!--a href="view/layout/applicationViewEOA.php"-->
+	
+					<td class="button"><?php echo $this->linkTo("home", "index", "Home"); ?> </td>
+		   
+		   
+					<!-- The below validation is to expose the below links only for people with role of admin or Coordinators -->
+					<td class="button"><?php echo $this->linkTo("boatramp", "index", "Boat Ramp"); ?> </td>
+					<td class="button"><?php echo $this->linkTo("waterbody", "index", "Waterbody"); ?> </td>
+				 
+					<!-- The below validation is to expose the below links only for people with role of admin or Coordinators -->
+					<?php if (isset($_SESSION['roleID']) && ($_SESSION['roleID'] < 3)) { ?> 
+					<td class="button"><?php echo $this->linkTo("user", "index", "Lake Host"); ?> </td>
+					<td class="button"><?php echo $this->linkTo("group", "index", "Groups"); ?> </td>
+					<?php } ?> 
 
-    <?php echo $this->linkTo("home", "index", "Home"); ?>
-    <!-- <li><?php // echo $this->linkTo("survey", "index", "Survey"); ?></li> -->
+					<td class="button"><?php echo $this->linkTo("surveysummary", "index", "Survey Summary"); ?></td>
+					<td class="button"><?php echo $this->linkTo("report", "index", "Report"); ?></td>
+ 
+					<!-- The below validation is to expose the below links only for people with role of admin -->
+					<?php if (isset($_SESSION['roleID']) && ($_SESSION['roleID'] < 2)) { ?> 
+					<!--<td class="button"><?php /* echo $this->linkTo("logging", "index", "Logging") */ ?> </td> -->
+					<?php } ?>
+					<td class="button"><?php echo $this->linkTo("sessionend", "end", "Logout"); ?> </td>
 
-    <!-- The below validation is to expose the below links only for people with role of admin or Coordinators -->
-    <?php echo $this->linkTo("boatramp", "index", "Boat Ramp"); ?>
-    <?php echo $this->linkTo("waterbody", "index", "Waterbody"); ?>
+				   <!-- <td class="button">	<?php /*echo "EOA OFF"*/ ?></td> <!-- SHOULD POINT HERE --> <!--a href="view/layout/applicationView.php"-->
 
-    <!-- The below validation is to expose the below links only for people with role of admin or Coordinators -->
-    <?php if (isset($_SESSION['roleID']) && ($_SESSION['roleID'] < 3)) { ?>
-        <?php echo $this->linkTo("user", "index", "Lake Host"); ?>
-        <?php echo $this->linkTo("group", "index", "Groups"); ?>
-    <?php } ?>
+</table>
+</div>
+
+</div>
+</body>
+</html>
+
+
 
     <!--  Drop Invasive Species -->
     <!-- <li><?php echo $this->linkTo("invasivespecies", "index", "Invasive Species"); ?></li>  -->
-
-    <?php echo $this->linkTo("surveysummary", "index", "Survey Summary"); ?>
-    <?php echo $this->linkTo("report", "index", "Report"); ?>
-
-    <!-- The below validation is to expose the below links only for people with role of admin -->
-    <?php if (isset($_SESSION['roleID']) && ($_SESSION['roleID'] < 2)) { ?>
-        <?php echo $this->linkTo("logging", "index", "Logging") ?>
-    <?php } ?>
-    <?php echo $this->linkTo("sessionend", "end", "Logout"); ?>
-
-    <?php echo "EOA OFF" ?> <!-- SHOULD POINT HERE --> <!--a href="view/layout/applicationView.php"-->
-
-
-</div>
+	
+	
+     <!-- <li><?php // echo $this->linkTo("survey", "index", "Survey"); ?> </li></li> -->
