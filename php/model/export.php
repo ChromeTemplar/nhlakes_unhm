756a87841ef9eadc781cccdbbdb83a2c52e295f0
ZacharyWallace <zacharywallace@gmail.com>
2015-12-15 20:08:54 -0500

    Added functionality to export all survey summaries performed by a group

--- php/model/export.php
+++ php/model/export.php
@@ -1,102 +1,150 @@
 <?php
 
-	//see http://php.net/manual/en/function.call-user-func-array.php how to use extensively
-	if(isset($_GET['runFunction']) && function_exists($_GET['runFunction']))
-	call_user_func($_GET['runFunction']);
-	else
-	echo "Function not found or wrong input";
+//see http://php.net/manual/en/function.call-user-func-array.php how to use extensively
+if(isset($_GET['runFunction']) && function_exists($_GET['runFunction']))
+call_user_func($_GET['runFunction']);
+else
+echo "Function not found or wrong input";
 
-	function exportAll (){
-		//set vars and set up connection	
-		$host="localhost";
-		$username="root";
-		$password="";
-		$databasename = "nhvbsr"; 
+function exportAll (){
+	//set vars and set up connection	
+	$host="localhost";
+	$username="root";
+	$password="";
+	$databasename = "nhvbsr"; 
+
+	$connection=mysql_connect($host,$username,$password); 
+
+	echo mysql_error();
+
+	$selectdb=mysql_select_db($databasename) or die("Database could not be selected"); 
+	$result=mysql_select_db($databasename)or die("database cannot be selected <br>");
+
+	//set up query
+	$output = "";
+	$table = "summary";
+	$sql = mysql_query("select * from $table");
+	$columns_total = mysql_num_fields($sql);
 	
-		$connection=mysql_connect($host,$username,$password); 
+	//get col names
+	for ($i = 0; $i < $columns_total; $i++) {
+	$heading = mysql_field_name($sql, $i);
+	$output .= '"'.$heading.'",';
+	}
+	$output .="\n";
 	
-		echo mysql_error();
-	
-		$selectdb=mysql_select_db($databasename) or die("Database could not be selected"); 
-		$result=mysql_select_db($databasename)or die("database cannot be selected <br>");
-	
-		//set up query
-		$output = "";
-		$table = "summary";
-		$sql = mysql_query("select * from $table");
-		$columns_total = mysql_num_fields($sql);
+	//get values
+	while ($row = mysql_fetch_array($sql)) {
+	for ($i = 0; $i < $columns_total; $i++) {
+	$output .='"'.$row["$i"].'",';
+	}
+	$output .="\n";
+	}
 		
-		//get col names
-		for ($i = 0; $i < $columns_total; $i++) {
-		$heading = mysql_field_name($sql, $i);
-		$output .= '"'.$heading.'",';
-		}
-		$output .="\n";
+	//ready file for download
+	$filename = "export.csv";
+	header('Content-type: application/csv');
+	header('Content-Disposition: attachment; filename='.$filename);
+	
+	echo $output;
+	exit;
+}
+
+function exportSingleSurvey () {
+
+	if (isset($_GET["summaryID"])) {
+		$id = $_GET["summaryID"];
+	}
+	
+	//set vars and set up connection	
+	$host="localhost";
+	$username="root";
+	$password="";
+	$databasename = "nhvbsr"; 
+
+	$connection=mysql_connect($host,$username,$password); 
+
+	echo mysql_error();
+
+	$selectdb=mysql_select_db($databasename) or die("Database could not be selected"); 
+	$result=mysql_select_db($databasename)or die("database cannot be selected <br>");
+
+	//set up query
+	$output = "";
+	$table = "summary";
+	$sql = mysql_query("select * from $table where id = $id");
+	$columns_total = mysql_num_fields($sql);
+	
+	//get col names
+	for ($i = 0; $i < $columns_total; $i++) {
+	$heading = mysql_field_name($sql, $i);
+	$output .= '"'.$heading.'",';
+	}
+	$output .="\n";
+	
+	//get values
+	while ($row = mysql_fetch_array($sql)) {
+	for ($i = 0; $i < $columns_total; $i++) {
+	$output .='"'.$row["$i"].'",';
+	}
+	$output .="\n";
+	}
 		
-		//get values
-		while ($row = mysql_fetch_array($sql)) {
-		for ($i = 0; $i < $columns_total; $i++) {
-		$output .='"'.$row["$i"].'",';
-		}
-		$output .="\n";
-		}
-			
-		//ready file for download
-		$filename = "export.csv";
+	//ready file for download
+	$filename = "export.csv";
 		header('Content-type: application/csv');
 		header('Content-Disposition: attachment; filename='.$filename);
 		
 		echo $output;
 		exit;
+}
+
+function exportGroupSurvey () {
+
+	if (isset($_GET["groupID"])) {
+		$id = $_GET["groupID"];
 	}
 	
-	function exportSingleSurvey () {
+	//set vars and set up connection	
+	$host="localhost";
+	$username="root";
+	$password="";
+	$databasename = "nhvbsr"; 
+
+	$connection=mysql_connect($host,$username,$password); 
+
+	echo mysql_error();
+
+	$selectdb=mysql_select_db($databasename) or die("Database could not be selected"); 
+	$result=mysql_select_db($databasename)or die("database cannot be selected <br>");
+
+	//set up query
+	$output = "";
+	$table = "summary";
+	$sql = mysql_query("select * from $table where lakeHostGroupID = $id");
+	$columns_total = mysql_num_fields($sql);
 	
-		if (isset($_GET["summaryID"])) {
-			$id = $_GET["summaryID"];
-		}
+	//get col names
+	for ($i = 0; $i < $columns_total; $i++) {
+	$heading = mysql_field_name($sql, $i);
+	$output .= '"'.$heading.'",';
+	}
+	$output .="\n";
+	
+	//get values
+	while ($row = mysql_fetch_array($sql)) {
+	for ($i = 0; $i < $columns_total; $i++) {
+	$output .='"'.$row["$i"].'",';
+	}
+	$output .="\n";
+	}
 		
-		//set vars and set up connection	
-		$host="localhost";
-		$username="root";
-		$password="";
-		$databasename = "nhvbsr"; 
-	
-		$connection=mysql_connect($host,$username,$password); 
-	
-		echo mysql_error();
-	
-		$selectdb=mysql_select_db($databasename) or die("Database could not be selected"); 
-		$result=mysql_select_db($databasename)or die("database cannot be selected <br>");
-	
-		//set up query
-		$output = "";
-		$table = "summary";
-		$sql = mysql_query("select * from $table where id = $id");
-		$columns_total = mysql_num_fields($sql);
-		
-		//get col names
-		for ($i = 0; $i < $columns_total; $i++) {
-		$heading = mysql_field_name($sql, $i);
-		$output .= '"'.$heading.'",';
-		}
-		$output .="\n";
-		
-		//get values
-		while ($row = mysql_fetch_array($sql)) {
-		for ($i = 0; $i < $columns_total; $i++) {
-		$output .='"'.$row["$i"].'",';
-		}
-		$output .="\n";
-		}
-			
-		//ready file for download
-		$filename = "export.csv";
+	//ready file for download
+	$filename = "export.csv";
 		header('Content-type: application/csv');
 		header('Content-Disposition: attachment; filename='.$filename);
 		
 		echo $output;
 		exit;
-	}
-
+}
 ?>
--- php/view/group/displayGroup.php
+++ php/view/group/displayGroup.php
@@ -1,11 +1,24 @@
-<?php //Display the correct form description ?>
-    <h1><?php echo $welcome; ?></h1>
-
-<?php
+<?php 
 
 //Take apart the url to get the group that was selected
 $idInURL = explode("&", explode("&id=", $_SERVER['REQUEST_URI'])[1]);
 $groupID = $idInURL[0];
+
+?>
+ 
+ <h1><?php echo $welcome; ?></h1>
+	
+<input type="button" id="test"  value="Export Group's Survey Summary Data"/>
+
+<script type="text/javascript">
+	var id = "<?php echo $groupID; ?>";
+	var url = "location.href='/nhvbsr/model/export.php?runFunction=exportGroupSurvey&groupID="+id+"';"
+
+    document.getElementById("test").setAttribute("onclick", url);
+</script>
+
+<?php
+
 //Build the table according to the selected group
 echo $this->buildTable($user, "group", $groupID);
 echo "(Coordinators denoted in green.)";
